b17f16fc860f71ca624999855aa6c782e3c7f242
Jabib Leiton <jabibleitonp@hotmail.com>
2014-06-26 11:57:07 -0600

    class14

---
+++ code/class14/class14_Seleccion/src/class14_seleccion/Class14_Seleccion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package class14_seleccion;
+
+/**
+ *
+ * @author k
+ */
+public class Class14_Seleccion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo={56,4,89,85,42};
+        int[] arregloOrdenado;
+        seleccion o=new seleccion();
+      arregloOrdenado=o.OrdenS(arreglo, "A");
+        for(int i=0;i<arregloOrdenado.length;i++){
+            System.out.println(arregloOrdenado[i]);
+        }
+            
+        
+    }
+    
+}
